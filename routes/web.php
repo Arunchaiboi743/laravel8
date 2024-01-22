06316c1923b85f52b46a96d28a042d2b43439b73
Arunchaiboi743 <152138023+Arunchaiboi743@users.noreply.github.com>
2024-01-22 12:22:49 +0700

    week 9

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Covid19Controller;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;  //เขียนเพิ่ม
+use App\Http\Controllers\UserController;  //เขียนเพิ่ม
+use App\Http\Controllers\VehicleController;  //เขียนเพิ่ม
 
 /*
 |--------------------------------------------------------------------------
@@ -100,3 +104,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+Route::resource('post', 'PostController');
+Route::resource('post', PostController::class);
+
+//Route::resource('profile', 'ProfileController');
+//Route::resource('user', 'UserController');
+//Route::resource('vehicle', 'VehicleController');
+
+Route::resource('profile', ProfileController::class);
+Route::resource('user', UserController::class);
+Route::resource('vehicle', VehicleController::class);
